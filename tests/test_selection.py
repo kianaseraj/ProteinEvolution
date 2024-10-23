01b8462cf242860b499c1659ae67090d2e6df9ca
Kiana <kianaseraj98@gmail.com>
2024-10-23 12:09:50 +0200

    Add a new test funciton, and improve documentation

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,41 +1,40 @@
-import pytest
 import pandas as pd
-import numpy as np
-import sys
-
-sys.path.insert(0,"/Users/kianaseraj/desktop/github-kianaseraj/ProteinEvolution/src")
-
 import Selection
-from Selection import MatingPool
-from Fitness import OptimizationFitness
 
 
-#A sample dataset for testing
+#A sample fitness score dataframe for testing
 data= {
-        'sequence': ['ATCG', 'GCTA', 'TGCA', 'ACGT', 'CGTA'],
+        'sequence': ['AT', 'GA', 'TGA', 'ACGT', 'CGTA'],
         'FitnessScore': [0.8, 0.6, 0.9, 0.7, 0.5]
     }
 sample_data = pd.DataFrame(data)
+length_threshold = 3
 
-
-#Testing keepfitest attribute of the selection class
-
-def test_keep_fittest_selects_half_population(sample_data):
+#Testing keepfittest method of Selection module; testing if the method keeps the half of the population
+def test_keep_fittest_selects_half_population(sample_data, length_threshold):
     select = Selection.Select()
-    result = select.KeepFittest(sample_data)
-    assert len(result.fittest_pop) == len(sample_data) / 2
+    result = select.KeepFittest(sample_data, length_threshold)
+    assert len(result.fittest_pop) == int(len(sample_data) / 2)
     
-def test_keep_fittest_selects_highest_scores(sample_data):
+#Testing keepfittest method of Selection module; testing if the module keeps the highestt scores  
+def test_keep_fittest_selects_highest_scores(sample_data, length_threshold):
     select = Selection.Select()
     result = select.KeepFittest(sample_data)
     assert result.fittest_pop['FitnessScore'].min() >= sample_data['FitnessScore'].median()
     
-def test_keep_fittest_shuffles_population(sample_data):
+#Testing keepfittest method of Selection module; testing if the module shuffle the data entries
+def test_keep_fittest_shuffles_population(sample_data, length_threshold):
     select = Selection.Select()
     result = select.KeepFittest(sample_data)
     assert not result.fittest_pop['sequence'].equals(sample_data['sequence'].sort_values(ascending=False))
-    
-
+ 
+  
+#Testing keepfittest method of Selection module; testing if the module selects sequences longer than a threshold
+def test_keep_fittest_select_threshold_sequences(sample_data, length_threshold):
+    select = Selection.Select()
+    result = select.KeepFittest(sample_data)
+    for seq in result["sequence"]:
+        assert len(seq) >=  length_threshold
 
 
 
