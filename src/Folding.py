c8278a26fb37327f12fe3e05f874679240e3523f
Kiana <kianaseraj98@gmail.com>
2024-10-30 09:05:44 +0100

    Add comments and documentation

--- src/Folding.py
+++ src/Folding.py
@@ -15,7 +15,7 @@ from biotite.structure.io.pdb import PDBFile
 
 """
 Second stage of the algorithm.
-The randomely generated population will be given plddt, pae, and ptm scores. 
+The randomely generated population from Individual module will be given plddt, pae, and ptm scores thorugh ESM language model. 
 The folding score results will be in pandas dataframe format.
 """
 @dataclass
@@ -49,15 +49,22 @@ class ESMFold(Predictor):
         self.model = None
 
     def load(self, dir_path : str) -> None:
-        """
-        loading ESMFold model
+        """Loading ESMFold model
+        Arguments:
+            - dir_path : A path to load the model in 
+        Returns: The loaded model will be put on a gpu and will be in an evaluation mode
         """
         torch.hub.set_dir(dir_path)
         self.model = esm.pretrained.esmfold_v1()
-        self.model = self.model.eval().cuda()
+        self.model = self.model.eval().cuda() #switch the model to an evaluation mode and move it to an available GPU
         return self.model
 
     def fold(self, Population : List[str]) -> FoldResult:
+        """Using ESMFold model to predict 3d atomic coorrdinates of protein sequences with their uncertainty scores
+        Arguments:
+            - Population : List of protein sequences for structure prediction by ESMFold
+        Returns: A dataframe of protein sequences with their atomic strructuer and their scores
+        """
         
         assert self.model is not None
         
@@ -68,21 +75,19 @@ class ESMFold(Predictor):
         atoms_ = []
         for sequence in Population:
             with torch.no_grad():
-                output = self.model.infer(sequence, chain_linker="X"*0, residue_index_offset=0)
-
-                output = tree_map(lambda x: x.to("cpu"), output)
+                output = self.model.infer(sequence, chain_linker="X"*0, residue_index_offset=0) #prerdict 3D sructure of sequences using ESMfold model.
+                output = tree_map(lambda x: x.to("cpu"), output) 
                 pdb_string = esm.esmfold.v1.misc.output_to_pdb(output)[0]
                 atoms : AtomArray = pdb_file_to_atomarray(StringIO(pdb_string))
-
-                plddt = output["plddt"]
+                plddt = output["plddt"] #second output of the ESMfold model, plddt score demonstrates the model's per residue confidence.
                 plddt = plddt[0, ...].numpy()
                 plddt = plddt.transpose()
                 plddt_.append(plddt[atom_order["CA"], :])
-                mean_plddt_.append((output["mean_plddt"]).numpy())
-                ptm_.append(float(output["ptm"]))
-                pae_.append(output["predicted_aligned_error"].numpy())
+                mean_plddt_.append((output["mean_plddt"]).numpy()) #obtain a mean plddt giving model's confidence on the whole sequence.
+                ptm_.append(float(output["ptm"])) #third output, ptm score represents the model's confidence in the overall predicted structure.
+                pae_.append(output["predicted_aligned_error"].numpy()) #fourth output, Pae score is a measure of how confident the model is in relative position of two residues within the predicted structure.
                 atoms_.append(atoms)
-        #turn the folding results into a dataframe 
+        #Store the obtained values from ESM model in a dataframe 
         atoms_df = pd.DataFrame({"sequence" : Population, "atoms": atoms_})
         plddt_df = pd.DataFrame({"sequence" : Population, "plddt" : plddt_})
         ptm_df = pd.DataFrame({"sequence" : Population, "ptm" : ptm_})
@@ -93,19 +98,21 @@ class ESMFold(Predictor):
 
 
 class ESMmodel(Predictor):
+    
     def __init__(self) -> None:
         super().__init__()
         self.model = None
         self.alphabet = None
 
     def load(self, dir_path : str, ) -> None:
+        """Loading ESM2 model, the model will predict masked amino acids in the Mutation module
+        Arguments: 
+            - dir_path : A directory path to load the model in
+        Returns : The model and its tokenization module, named as alphabet, will be put on GPU for inference tasks
         """
-        loading ESM2 model, used in masked amino acid prediction
-        """
-
         torch.hub.set_dir(dir_path)
-        self.model, self.alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-        self.model.eval()
+        self.model, self.alphabet = esm.pretrained.esm2_t33_650M_UR50D() #Load the model and alphabet module. alphabt module will be used to tokenize squence inputs
+        self.model.eval() #Set the model to evaluation mode
         return self.model, self.alphabet
 
     def fold(self, sequence : str) -> FoldResult:
