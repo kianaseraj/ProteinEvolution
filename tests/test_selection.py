f29a79e8751dd61253a9a63b59c89e2a6c0fe53e
Kiana <kianaseraj98@gmail.com>
2024-11-26 15:51:27 +0330

    Improve documentation of functions by adding docstrings

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -10,29 +10,55 @@ data= {
 sample_data = pd.DataFrame(data)
 length_threshold = 3
 
-#Testing keepfittest method of Selection module; testing if the method keeps the half of the population
+
 def test_keep_fittest_selects_half_population(sample_data, length_threshold):
+    """Test the KeepFittest method of the Selection module to ensure that it selects half of the population.
+        
+    Arguments:
+        -sample_data (pd.DataFrame): The dataframe containing sequences and their corresponding fitness scores.
+        -length_threshold (int): The threshold length for selecting sequences.
+    """
     select = Selection.Select()
     result = select.KeepFittest(sample_data, length_threshold)
     assert len(result.fittest_pop) == int(len(sample_data) / 2)
     
-#Testing keepfittest method of Selection module; testing if the module keeps the highestt scores  
+
 def test_keep_fittest_selects_highest_scores(sample_data, length_threshold):
+    """Test the KeepFittest method of the Selection module to ensure it selects sequences with the highest fitness scores.
+   
+    Arguments:
+        -sample_data (pd.DataFrame): The dataframe containing sequences and their corresponding fitness scores.
+        -length_threshold (int): The threshold length for selecting sequences.
+    """
     select = Selection.Select()
-    result = select.KeepFittest(sample_data)
+    result = select.KeepFittest(sample_data, length_threshold)
     assert result.fittest_pop['FitnessScore'].min() >= sample_data['FitnessScore'].median()
     
-#Testing keepfittest method of Selection module; testing if the module shuffle the data entries
+
 def test_keep_fittest_shuffles_population(sample_data, length_threshold):
+    
+    """Test the KeepFittest method of the Selection module to ensure that it shuffles the population.
+  
+    Argumentss:
+        -sample_data (pd.DataFrame): The dataframe containing sequences and their corresponding fitness scores.
+        -length_threshold (int): The threshold length for selecting sequences.
+
+    """
     select = Selection.Select()
-    result = select.KeepFittest(sample_data)
+    result = select.KeepFittest(sample_data, length_threshold)
     assert not result.fittest_pop['sequence'].equals(sample_data['sequence'].sort_values(ascending=False))
  
   
-#Testing keepfittest method of Selection module; testing if the module selects sequences longer than a threshold
 def test_keep_fittest_select_threshold_sequences(sample_data, length_threshold):
+    
+    """Test the KeepFittest method of the Selection module to ensure that it selects sequences longer than a specified threshold.
+  
+    Arguments:
+        -sample_data (pd.DataFrame): The dataframe containing sequences and their corresponding fitness scores.
+        -length_threshold (int): The threshold length for selecting sequences.
+    """
     select = Selection.Select()
-    result = select.KeepFittest(sample_data)
+    result = select.KeepFittest(sample_data, length_threshold)
     for seq in result["sequence"]:
         assert len(seq) >=  length_threshold
 
